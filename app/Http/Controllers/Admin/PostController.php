308467fa8b6db977a38a6b69015a51322a35f847
Piergiorgio Piras <piraspiergiorgio5@gmail.com>
2022-05-24 13:51:49 +0200

    add categories in posts.edit

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -108,7 +108,13 @@ class PostController extends Controller
     public function edit(Post $post)
     {
         if (Auth::user()->id !== $post->user_id) abort(403);
-        return view('admin.posts.edit', compact('post'));
+        $categories = Category::all();
+        return view('admin.posts.edit', [
+            'post'          => $post,
+            'categories'    => $categories
+        ]);
+
+        
     }
 
     /**
--- resources/views/admin/posts/edit.blade.php
+++ resources/views/admin/posts/edit.blade.php
@@ -28,6 +28,17 @@
                     <input type="text" class="form-control" id="slug" name="slug" value="{{ $post->slug }}">
                 </div>
                 <div class="mb-3">
+                    <select class="form-select" name="category_id" id="category">
+                        <option value="" selected>Seleziona una categoria</option>
+                        @foreach ($categories as $category)
+                            <option value="{{ $category->id }}"
+                                @if ($category->id == old('category_id', $post->category->id)) selected @endif>
+                                    {{ $category->type }}
+                            </option>
+                        @endforeach
+                    </select>
+                </div>
+                <div class="mb-3">
                     <label for="description" class="form-label">Descrizione</label>
                     <textarea class="form-control" name="description" id="description">{{ $post->description }}</textarea>
                 </div>
